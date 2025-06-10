d9560108740673bab2f6e943cf89b1e0c6e910e3
Jorge Rodriguez <jorgerr76@hotmail.com>
2025-06-10 11:36:31 +0200

    feat: aria label add

--- src/js/components/Card.js
+++ src/js/components/Card.js
@@ -24,9 +24,9 @@ export class Card {
           <strong class="card__price-amount">${this.data.price} €</strong>
           </div>
           
-          <button class="card__see-details"><span>Ver details </span><img src="./img/chevron-down.svg" class="card__see-details-icon"></button>
+          <button aria-label="Ver detalles" class="card__see-details"><span>Ver detalles </span><img src="./img/chevron-down.svg" class="card__see-details-icon"></button>
         </span>
-        <button class="card__button-reserve">Reservar</button>
+        <button aria-label="Reservar" class="card__button-reserve">Reservar</button>
       </footer>
     `;
 
